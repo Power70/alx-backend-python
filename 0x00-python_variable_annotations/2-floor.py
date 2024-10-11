6eef81d3be70ffd336781868d764c60e177d338a
vikmaamaa <nwisuanu@gmail.com>
2024-10-11 05:11:33 +0100

    initial commit

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Type-annotated function floor which takes a float n as argument and returns
-the floor of the float."""
-
-
-def floor(n: float) -> int:
-    """Return the floor of the float."""
-    return int(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Type-annotated function to_str that takes a float n as argument and returns
-the string representation of the float."""
-from typing import Union
-
-
-def to_str(n: float) -> str:
-    """Return the string representation of the float."""
-    return str(n)
